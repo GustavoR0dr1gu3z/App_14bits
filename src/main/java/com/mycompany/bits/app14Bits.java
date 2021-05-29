48fe854825b4ff2fac32a12890cc484056d1e910
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-28 22:25:37 -0500

    Diseñando Interfaz

--- src/main/java/com/mycompany/bits/app14Bits.java
+++ src/main/java/com/mycompany/bits/app14Bits.java
@@ -36,15 +36,18 @@ public class app14Bits extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Aceptar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jTextField1.setText("jTextField1");
 
         jLabel1.setText("MOVLW");
 
         jLabel2.setText("Digite una instruccion de 3 bits válida");
-
-        jLabel3.setText("jLabel3");
 
         jLabel4.setText("Resultado: ");
 
@@ -91,6 +94,10 @@ public class app14Bits extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
