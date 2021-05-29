26e4617ebe1ade0f6b43953b2c5181911d5ea420
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-05-28 22:26:01 -0500

    Diseñando Interfaz

--- src/main/java/com/mycompany/bits/app14Bits.java
+++ src/main/java/com/mycompany/bits/app14Bits.java
@@ -44,6 +44,11 @@ public class app14Bits extends javax.swing.JFrame {
         });
 
         jTextField1.setText("jTextField1");
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("MOVLW");
 
@@ -98,6 +103,10 @@ public class app14Bits extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     /**
      * @param args the command line arguments
